dff1d2c8ce34b7b22a0873995a9a787baa960821
Matthew Hellige <mhockeyz@yahoo.com>
2016-07-22 17:44:57 -0500

    Add files via upload

---
+++ minecraft.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+case "$1" in
+  start)
+    echo "Starting Minecraft server on screen Minecraft..."
+    screen -S Minecraft /minecraft/startscript.sh
+    echo "Done!"
+    ;;
+  stop)
+    echo "Backing up the Minecraft server..."
+    cp -r /minecraft/world /minecraft/backup
+    echo "Done!"
+
+    echo "The last backup was on $date." >> /minecraft/backup/date.txt
+
+    echo "The server is shutting down."
+    screen -X -S Minecraft kill 
+   ;;
+  *)
+    echo "Usage: {start|stop}"
+    exit 1
+    ;;
+esac
